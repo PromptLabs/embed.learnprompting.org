56b1efe2850cc9679e1d65db38b173b95642e7b7
Wesley Smith <me@wesjd.net>
2023-03-06 13:33:35 -0500

    move theme to object

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { MantineProvider } from "@mantine/core"
+import { MantineProvider, MantineThemeOverride } from "@mantine/core"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
@@ -16,16 +16,14 @@ const router = createBrowserRouter([
     },
 ])
 
+const theme: MantineThemeOverride = {
+    colorScheme: "light",
+    fontFamily: `system-ui,-apple-system,Segoe UI,Roboto,Ubuntu,Cantarell,Noto Sans,sans-serif,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`,
+}
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <MantineProvider
-            withGlobalStyles
-            withNormalizeCSS
-            theme={{
-                colorScheme: "light",
-                fontFamily: `system-ui,-apple-system,Segoe UI,Roboto,Ubuntu,Cantarell,Noto Sans,sans-serif,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`,
-            }}
-        >
+        <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
             <RouterProvider router={router} />
         </MantineProvider>
     </React.StrictMode>
