7bbee189b0ba16ff7af3ef935110782a89858869
Wesley Smith <me@wesjd.net>
2023-03-06 14:45:31 -0500

    switch from AppShell to regular flexbox

--- src/pages/EmbedPage.tsx
+++ src/pages/EmbedPage.tsx
@@ -1,33 +1,64 @@
-import { Anchor, AppShell, Flex, Footer, Space, Text } from "@mantine/core"
+import { Anchor, AppShell, Box, Flex, Mark, ScrollArea, Space, Text, Textarea } from "@mantine/core"
 
-const ShellFooter = ({ editUrl }: { editUrl: string }) => {
+const PromptEditor = () => {
     return (
-        <Footer height="fit-content">
-            <Flex direction="row" p="sm" td="underline">
-                <Text component="a" href="https://learnprompting.org" target="_blank">
-                    learnprompting.org
-                </Text>
-                <Space ml="auto" />
-                <Text component="a" href={editUrl} target="_blank">
-                    edit this embed
-                </Text>
+        <>
+            <Text>Prompt</Text>
+            <textarea style={{ flexBasis: "100%", flexShrink: 1, boxSizing: "border-box" }} />
+        </>
+    )
+}
+
+const OutputDisplay = ({ output }: { output: string }) => {
+    return (
+        <>
+            <Text>Output</Text>
+            <Mark
+                color="cyan"
+                w="fit-content"
+                style={{ overflowWrap: "break-word", whiteSpace: "pre-wrap", boxDecorationBreak: "clone" }}
+            >
+                {output}
+            </Mark>
+        </>
+    )
+}
+
+const Playground = () => {
+    return (
+        <Flex direction="row" p="sm" h="100%">
+            <Flex direction="column" w="50%">
+                <PromptEditor />
             </Flex>
-        </Footer>
+            <Space p="xs" />
+            <Flex direction="column" w="50%">
+                <OutputDisplay output="test output test output test output test output test output test output test output test output test output test output test output test output test output " />
+            </Flex>
+        </Flex>
+    )
+}
+
+const Footer = ({ editUrl }: { editUrl: string }) => {
+    return (
+        <Flex direction="row" p="sm" td="underline">
+            <Text component="a" href="https://learnprompting.org" target="_blank">
+                learnprompting.org
+            </Text>
+            <Space ml="auto" />
+            <Text component="a" href={editUrl} target="_blank">
+                edit this embed
+            </Text>
+        </Flex>
     )
 }
 
 const EmbedPage = () => {
     return (
-        <AppShell footer={<ShellFooter editUrl="lol.com" />}>
-            <Flex direction="row">
-                <Flex direction="column" style={{ flexGrow: 1 }}>
-                    <Text>Prompt</Text>
-                </Flex>
-                <Flex direction="column" style={{ flexGrow: 1 }}>
-                    <Text>Output</Text>
-                </Flex>
-            </Flex>
-        </AppShell>
+        <Flex direction="column" h="100vh">
+            <Playground />
+            <Space mt="auto" />
+            <Footer editUrl="lol.com" />
+        </Flex>
     )
 }
 
