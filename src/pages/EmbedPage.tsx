00f9f985e4068758ed8d0873df06848b1be39be7
Wesley Smith <me@wesjd.net>
2023-03-06 13:54:13 -0500

    embed page layout

--- src/pages/EmbedPage.tsx
+++ src/pages/EmbedPage.tsx
@@ -1,7 +1,34 @@
-import { Text } from "@mantine/core"
+import { Anchor, AppShell, Flex, Footer, Space, Text } from "@mantine/core"
+
+const ShellFooter = ({ editUrl }: { editUrl: string }) => {
+    return (
+        <Footer height="fit-content">
+            <Flex direction="row" p="sm" td="underline">
+                <Text component="a" href="https://learnprompting.org" target="_blank">
+                    learnprompting.org
+                </Text>
+                <Space ml="auto" />
+                <Text component="a" href={editUrl} target="_blank">
+                    edit this embed
+                </Text>
+            </Flex>
+        </Footer>
+    )
+}
 
 const EmbedPage = () => {
-    return <Text>Embed Page</Text>
+    return (
+        <AppShell footer={<ShellFooter editUrl="lol.com" />}>
+            <Flex direction="row">
+                <Flex direction="column" style={{ flexGrow: 1 }}>
+                    <Text>Prompt</Text>
+                </Flex>
+                <Flex direction="column" style={{ flexGrow: 1 }}>
+                    <Text>Output</Text>
+                </Flex>
+            </Flex>
+        </AppShell>
+    )
 }
 
 export default EmbedPage
