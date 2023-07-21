6e288c48777972ec11cf83519b0e0d178957a68d
trigaten <sanderschulhoff@gmail.com>
2023-07-21 10:25:34 +0300

    feat: add chat models to list

--- src/urlconfig.ts
+++ src/urlconfig.ts
@@ -1,12 +1,19 @@
 import * as yup from "yup"
 
 export const MODELS = [
+    "gpt-4",
+    "gpt-4-0613",
+    "gpt-4-32k",
+    "gpt-4-32k-0613",
+    "gpt-3.5-turbo",
+    "gpt-3.5-turbo-16k",
+    "gpt-3.5-turbo-0613",
+    "gpt-3.5-turbo-16k-0613",
     "text-davinci-003",
     "text-davinci-002",
-    "text-davinci-001",
     "text-curie-001",
     "text-babbage-001",
-    "text-ada-001",
+    "text-ada-001"
 ]
 
 export const urlConfigSchema = yup.object({
